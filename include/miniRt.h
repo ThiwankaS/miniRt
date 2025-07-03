18580f5fdf17f4e9bc310c7bfded43beb33f2c10
Thiwanka Somachandra <thivankas@outlook.com>
2025-07-04 02:17:38 +0300

    importing all necessary standard librariesand make sure all working

--- include/miniRt.h
+++ include/miniRt.h
@@ -1,8 +1,16 @@
 # ifndef FT_MINIRT_H
 # define FT_MINIRT_H
+# define _GNU_SOURCE
 
+# include <math.h>
+# include <stdlib.h>
 # include <stdio.h>
+# include <stdbool.h>
+# include <stdint.h>
+# include <fcntl.h>
 # include <MLX42/MLX42.h>
 # include "../ft_libft/libft.h"
+# include "../ft_libft/ft_printf.h"
+# include "../ft_libft/get_next_line.h"
 
 #endif
--- main.c
+++ main.c
@@ -2,6 +2,22 @@
 
 int main(void)
 {
+	ft_printf("hey, bro !\n");
+
+	double result = M_PI / 2;
+	printf("result : %.5f\n", result);
+
+	char *line = NULL;
+	int fd = open("test.txt", R_OK, 0644);
+	if(fd < 0)
+		return 0;
+	line = get_next_line(fd);
+	while(line != NULL)
+	{
+		ft_printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
 	const char *msg = "Hello, world!";
 	printf("%s length is %zu\n", msg, ft_strlen(msg));
 
