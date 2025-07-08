1d16ea0a8156af6cf4ebf18338b48260703804de
Thiwanka Somachandra <thivankas@outlook.com>
2025-07-08 20:27:56 +0300

    adding comments for more clarity

--- srcs/tuples/operations1.c
+++ srcs/tuples/operations1.c
@@ -1,6 +1,22 @@
-# include "../../include/miniRt.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations1.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 20:21:24 by tsomacha          #+#    #+#             */
+/*   Updated: 2025/07/08 20:25:58 by tsomacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void tuple_add(t_tuple *t0, t_tuple *t1, t_tuple *t2)
+#include "../../include/miniRt.h"
+
+/**
+ * Adds corresponding components of two tuples (t1 and t2)
+ * and stores the result in t0.
+ */
+void	tuple_add(t_tuple *t0, t_tuple *t1, t_tuple *t2)
 {
 	t0->t[0] = t1->t[0] + t2->t[0];
 	t0->t[1] = t1->t[1] + t2->t[1];
@@ -8,7 +24,11 @@ void tuple_add(t_tuple *t0, t_tuple *t1, t_tuple *t2)
 	t0->t[3] = t1->t[3] + t2->t[3];
 }
 
-void tuple_subtract(t_tuple *t0, t_tuple *t1, t_tuple *t2)
+/**
+ * Subtracts components of tuple t2 from tuple t1
+ * and stores the result in t0.
+ */
+void	tuple_subtract(t_tuple *t0, t_tuple *t1, t_tuple *t2)
 {
 	t0->t[0] = t1->t[0] - t2->t[0];
 	t0->t[1] = t1->t[1] - t2->t[1];
@@ -16,14 +36,23 @@ void tuple_subtract(t_tuple *t0, t_tuple *t1, t_tuple *t2)
 	t0->t[3] = t1->t[3] - t2->t[3];
 }
 
-void tuple_negate(t_tuple *t1, t_tuple *t2)
+/**
+ * Negates a tuple by subtracting it from a zero vector.
+ * Stores the result in t1.
+ */
+void	tuple_negate(t_tuple *t1, t_tuple *t2)
 {
-	t_tuple zero;
+	t_tuple	zero;
+
 	vector(&zero, 0.0, 0.0, 0.0);
 	tuple_subtract(t1, &zero, t2);
 }
 
-void tuple_multiply_scalor(t_tuple *t0, t_tuple *t1, float a)
+/**
+ * Multiplies each component of tuple t1 by scalar 'a'
+ * and stores the result in t0.
+ */
+void	tuple_multiply_scalor(t_tuple *t0, t_tuple *t1, float a)
 {
 	t0->t[0] = t1->t[0] * a;
 	t0->t[1] = t1->t[1] * a;
@@ -31,11 +60,15 @@ void tuple_multiply_scalor(t_tuple *t0, t_tuple *t1, float a)
 	t0->t[3] = t1->t[3] * a;
 }
 
-void tuple_divide_scalor(t_tuple *t0, t_tuple *t1, float a)
+/**
+ * Divides each component of tuple t1 by scalar 'a'
+ * and stores the result in t0. If 'a' is zero, t0 is set to zero tuple.
+ */
+void	tuple_divide_scalor(t_tuple *t0, t_tuple *t1, float a)
 {
-	float b;
+	float	b;
 
-	if( a == 0.0)
+	if (a == 0.0)
 	{
 		t0->t[0] = 0.0;
 		t0->t[1] = 0.0;
