7c70fd62dbd7b7b857e3cb6044a377e827d64de6
Thiwanka Somachandra <thivankas@outlook.com>
2025-07-08 09:22:00 +0300

    implementing matices

--- include/matrix.h
+++ include/matrix.h
@@ -1,6 +1,16 @@
 # ifndef FT_MATRIX_H
 # define FT_MATRIX_H
 
+# define SIZE 4
+
 # include "miniRt.h"
 
+typedef struct s_mat
+{
+	float m[SIZE][SIZE];
+} t_mat;
+
+void matrix(t_mat *m);
+void matrix_print(t_mat *mt);
+void matrix_multiply(t_mat *m0, t_mat *m1, t_mat *m2);
 # endif
--- srcs/matrices/operations1.c
+++ srcs/matrices/operations1.c
@@ -1,2 +1,65 @@
 # include "../../include/miniRt.h"
 
+void matrix(t_mat *m)
+{
+	int row;
+	int column;
+
+	row = 0;
+	column = 0;
+	while(column < SIZE)
+	{
+		row = 0;
+		while(row < SIZE)
+		{
+			m->m[column][row] = 0.0;
+			row++;
+		}
+		column++;
+	}
+}
+
+void matrix_print(t_mat *mt)
+{
+	int row;
+	int column;
+
+	row = 0;
+	column = 0;
+	while(column < SIZE)
+	{
+		row = 0;
+		while(row < SIZE)
+		{
+			printf("m[%d][%d] : %.2f\t", column, row, mt->m[column][row]);
+			row++;
+		}
+		printf("\n");
+		column++;
+	}
+}
+
+void matrix_multiply(t_mat *m0, t_mat *m1, t_mat *m2)
+{
+	int row;
+	int column;
+	int k;
+
+	row = 0;
+	column = 0;
+	k = 0;
+	while(column < SIZE)
+	{
+		row = 0;
+		while(row < SIZE)
+		{
+			while(k < SIZE)
+			{
+				m0->m[column][row] += m1->m[column][k] + m2->m[k][row];
+				k++;
+			}
+			row++;
+		}
+		column++;
+	}
+}
