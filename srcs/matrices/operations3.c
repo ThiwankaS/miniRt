f077d2e3fab0d622b59c6ca5007d544298a069d1
Thiwanka Somachandra <thivankas@outlook.com>
2025-07-08 21:01:49 +0300

    adding comment for more clarity

--- srcs/matrices/operations3.c
+++ srcs/matrices/operations3.c
@@ -1,56 +1,86 @@
-# include "../../include/miniRt.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations3.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 20:54:00 by tsomacha          #+#    #+#             */
+/*   Updated: 2025/07/08 20:59:58 by tsomacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-float matrix_cofactor(t_mat3 *m0, int row, int column)
+#include "../../include/miniRt.h"
+
+/**
+ * Computes the cofactor of a 3x3 matrix at the specified row and column.
+ * Applies sign based on position: cofactor = ±minor depending on (row + column).
+ */
+float	matrix_cofactor(t_mat3 *m0, int row, int column)
 {
-	float det;
+	float	det;
 
 	det = minor(m0, row, column);
-	if((row + column) % 2 != 0)
+	if ((row + column) % 2 != 0)
 		det = det * -1;
-	return det;
+	return (det);
 }
 
-float det3x3(t_mat3 *m)
+/**
+ * Computes the determinant of a 3x3 matrix using cofactor expansion.
+ */
+float	det3x3(t_mat3 *m)
 {
-	float det;
+	float	det;
 
 	det = 0.0;
 	det += m->m[0][0] * matrix_cofactor(m, 0, 0);
 	det += m->m[0][1] * matrix_cofactor(m, 0, 1);
 	det += m->m[0][2] * matrix_cofactor(m, 0, 2);
-	return det;
+	return (det);
 }
 
-float det4x4(t_mat *m)
+/**
+ * Computes the determinant of a 4x4 matrix using cofactor expansion along
+ * the first row.
+ */
+float	det4x4(t_mat *m)
 {
-	float det = 0.0;
-	t_mat3 sub;
-	int j;
+	int		j;
+	float	det;
+	float	cofactor;
+	t_mat3	sub;
 
 	j = 0;
+	det = 0.0;
 	while (j < SIZE)
 	{
 		submatrix4x4(&sub, m, 0, j);
-		float cofactor = det3x3(&sub);
+		cofactor = det3x3(&sub);
 		if (j % 2 != 0)
 			cofactor = -cofactor;
 		det += m->m[0][j] * cofactor;
 		j++;
 	}
-	return det;
+	return (det);
 }
 
-bool matrix_inverse(t_mat *out, t_mat *in)
+/**
+ * Calculates the inverse of a 4x4 matrix using the adjugate and determinant.
+ * Returns true if the inverse exists, false if the matrix is
+ * non-invertible (det = 0).
+ */
+bool	matrix_inverse(t_mat *out, t_mat *in)
 {
-	int row;
-	int col;
-	float det;
-	float cofactor;
-	t_mat3 sub;
+	int		row;
+	int		col;
+	float	det;
+	float	cofactor;
+	t_mat3	sub;
 
 	det = det4x4(in);
 	if (det == 0.0)
-		return false;
+		return (false);
 	row = 0;
 	while (row < SIZE)
 	{
@@ -66,6 +96,5 @@ bool matrix_inverse(t_mat *out, t_mat *in)
 		}
 		row++;
 	}
-	return true;
+	return (true);
 }
-
