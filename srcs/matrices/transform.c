474f83a8e654c5e4d7e7035223ef2e906b8ef6bd
Thiwanka Somachandra <thivankas@outlook.com>
2025-07-27 02:31:12 +0300

    refactor for norminette stds

--- srcs/matrices/transform.c
+++ srcs/matrices/transform.c
@@ -6,7 +6,7 @@
 /*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 06:04:34 by tsomacha          #+#    #+#             */
-/*   Updated: 2025/07/11 04:20:59 by tsomacha         ###   ########.fr       */
+/*   Updated: 2025/07/27 02:30:40 by tsomacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@
  */
 t_mat	translation(float x, float y, float z)
 {
-	t_mat m = identity();
+	t_mat	m;
+
+	m = identity();
 	m.m[0][3] = x;
 	m.m[1][3] = y;
 	m.m[2][3] = z;
@@ -25,11 +27,14 @@ t_mat	translation(float x, float y, float z)
 }
 
 /**
- * Creates and returns a scaling matrix that scales along x, y, and z axes.
+ * Creates and returns a scaling matrix that scales
+ * along x, y, and z axes.
  */
 t_mat	scaling(float x, float y, float z)
 {
-	t_mat m = identity();
+	t_mat	m;
+
+	m = identity();
 	m.m[0][0] = x;
 	m.m[1][1] = y;
 	m.m[2][2] = z;
@@ -37,14 +42,18 @@ t_mat	scaling(float x, float y, float z)
 }
 
 /**
- * Creates and returns a rotation matrix around the X-axis by angle r (in radians).
+ * Creates and returns a rotation matrix around the X-axis
+ * by angle r (in radians).
  */
 t_mat	rotate_x(float r)
 {
-	t_mat m = identity();
-	float v1 = cos(r);
-	float v2 = sin(r);
+	t_mat	m;
+	float	v1;
+	float	v2;
 
+	m = identity();
+	v1 = cos(r);
+	v2 = sin(r);
 	m.m[1][1] = v1;
 	m.m[1][2] = -v2;
 	m.m[2][1] = v2;
@@ -53,14 +62,18 @@ t_mat	rotate_x(float r)
 }
 
 /**
- * Creates and returns a rotation matrix around the Y-axis by angle r (in radians).
+ * Creates and returns a rotation matrix around the Y-axis
+ * by angle r (in radians).
  */
 t_mat	rotate_y(float r)
 {
-	t_mat m = identity();
-	float v1 = cos(r);
-	float v2 = sin(r);
+	t_mat	m;
+	float	v1;
+	float	v2;
 
+	m = identity();
+	v1 = cos(r);
+	v2 = sin(r);
 	m.m[0][0] = v1;
 	m.m[0][2] = v2;
 	m.m[2][0] = -v2;
@@ -69,18 +82,21 @@ t_mat	rotate_y(float r)
 }
 
 /**
- * Creates and returns a rotation matrix around the Z-axis by angle r (in radians).
+ * Creates and returns a rotation matrix around the Z-axis
+ * by angle r (in radians).
  */
 t_mat	rotate_z(float r)
 {
-	t_mat m = identity();
-	float v1 = cos(r);
-	float v2 = sin(r);
+	t_mat	m;
+	float	v1;
+	float	v2;
 
+	m = identity();
+	v1 = cos(r);
+	v2 = sin(r);
 	m.m[0][0] = v1;
 	m.m[0][1] = -v2;
 	m.m[1][0] = v2;
 	m.m[1][1] = v1;
 	return (m);
 }
-
