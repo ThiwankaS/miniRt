9b6b2f1599d835ea3e6c24db24b61afe333be5f4
Amarathun Hettiarachchi Wickramage Oshintha Sandaruwan <aoshinth@c2r4p11.hive.fi>
2025-07-30 12:07:56 +0300

    modified norminette

--- include/graphics.h
+++ include/graphics.h
@@ -6,7 +6,7 @@
 /*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 11:39:48 by aoshinth          #+#    #+#             */
-/*   Updated: 2025/07/30 11:52:16 by aoshinth         ###   ########.fr       */
+/*   Updated: 2025/07/30 12:06:36 by aoshinth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,39 +52,39 @@ typedef struct s_world
 	t_light		light;
 	t_object	*components;
 	int			obj_count;
-} 	t_world;
+}	t_world;
 
 typedef struct s_compute
 {
-	bool inside;
-	bool shadowed;
-	float value;
-	t_object *object;
-	t_tuple p;
-	t_tuple over_p;
-	t_tuple eye_v;
-	t_tuple normal_v;
-} t_compute;
+	bool		inside;
+	bool		shadowed;
+	float		value;
+	t_object	*object;
+	t_tuple		p;
+	t_tuple		over_p;
+	t_tuple		eye_v;
+	t_tuple		normal_v;
+}	t_compute;
 
 typedef struct s_camera
 {
-	int hsize;
-	int vsize;
-	float fov;
-	float half_width;
-	float half_height;
-	float pixel_size;
-	bool set_camera;
-	t_mat transform;
-	t_mat invs;
-} t_camera;
+	int		hsize;
+	int		vsize;
+	float	fov;
+	float	half_width;
+	float	half_height;
+	float	pixel_size;
+	bool	set_camera;
+	t_mat	transform;
+	t_mat	invs;
+}	t_camera;
 
 typedef struct s_hit
 {
 	float		t;
 	t_object	*object;
 	bool		hit;
-} t_hit;
+}	t_hit;
 
 typedef struct s_state
 {
@@ -98,32 +98,34 @@ typedef struct s_state
 	bool		done;
 }	t_state;
 
-void position(t_tuple *pp, t_ray *r, float t);
+void	position(t_tuple *pp, t_ray *r, float t);
 t_hit	find_hit(t_world *world, t_ray *ray);
 
 void	creating_plane_object(t_object *s);
 void	creating_sphere_object(t_object *s);
 
-void find_hit_cylinder(t_object *object, t_ray *r, t_hit *h);
-void hit_cap(t_object *object, t_ray *r, t_hit *h);
-void find_hit_plane(t_object *object, t_ray *r, t_hit *h);
-void find_hit_sphere(t_object *object, t_ray *r, t_hit *h);
-void set_transform(t_mat *m1, t_mat *m2);
-uint32_t tuple_to_color(t_tuple *tp);
+void	find_hit_cylinder(t_object *object, t_ray *r, t_hit *h);
+void	hit_cap(t_object *object, t_ray *r, t_hit *h);
+void	find_hit_plane(t_object *object, t_ray *r, t_hit *h);
+void	find_hit_sphere(t_object *object, t_ray *r, t_hit *h);
+void	set_transform(t_mat *m1, t_mat *m2);
+uint32_t	tuple_to_color(t_tuple *tp);
 
 t_tuple	normal_at(t_object *s, t_tuple *world_point);
-t_tuple reflect(t_tuple *in, t_tuple *normal);
+t_tuple	reflect(t_tuple *in, t_tuple *normal);
 t_tuple	lighting(t_object *obj, t_light *light, t_compute *comp);
 
-t_compute	prepare_compute(float t, t_object *object, t_ray *r, t_world *world);
+t_compute	prepare_compute(float t, t_object *object, t_ray *r,
+			t_world *world);
 t_tuple	color_at(t_world *world, t_ray *r);
-void view_trans(t_camera *camera, t_tuple *from, t_tuple *to, t_tuple *up);
-t_ray ray_for_pixel(t_camera *camera, int px, int py);
+void	view_trans(t_camera *camera, t_tuple *from, t_tuple *to, t_tuple *up);
+t_ray	ray_for_pixel(t_camera *camera, int px, int py);
 
-void handle_drag(void *param);
-t_object *pick_object_at(int px, int py, t_camera *camera, t_world *world);
-void key_handler(mlx_key_data_t keydata, void *param);
-void mouse_handler(mouse_key_t button, action_t action, modifier_key_t mods, void *param);
+void	handle_drag(void *param);
+t_object	*pick_object_at(int px, int py, t_camera *camera, t_world *world);
+void	key_handler(mlx_key_data_t keydata, void *param);
+void	mouse_handler(mouse_key_t button, action_t action, modifier_key_t mods,
+			void *param);
 
 void	update_fov(t_camera *camera, float fov);
 void	render_if_resized(void *param);
@@ -131,9 +133,9 @@ void	render_if_resized(void *param);
 void	render(t_state *state);
 void	keypress(mlx_key_data_t keydata, void *param);
 void	resize_selected_object(mlx_key_data_t keydata, t_state *state);
-t_object *pick_object_at(int px, int py, t_camera *camera, t_world *world);
-int change_value(mlx_key_data_t keydata, t_object *selected);
+t_object	*pick_object_at(int px, int py, t_camera *camera, t_world *world);
+int		change_value(mlx_key_data_t keydata, t_object *selected);
 void	handle_resize(t_state *state, int32_t cur_w, int32_t cur_h);
 void	select_next_object(t_state *state);
 
-# endif
+#endif
